45669da85f068324760a3245f1c4bf4ec3a14e8e
Madelyn Romero <m.romero013@gmail.com>
2020-01-30 12:16:29 -0700

    Fix typo

--- spec/requests/api/v1/customer_request_spec.rb
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -16,14 +16,14 @@ describe "Customer API" do
   end
 
   it "sends single customer by id" do
-    customer = create(:customer)
+    customer_1 = create(:customer)
 
-    get "/api/v1/customers/#{customer.id}"
+    get "/api/v1/customers/#{customer_1.id}"
 
     customer = JSON.parse(response.body)['data']['attributes']
 
     expect(response).to be_successful
-    expect(customer['first_name']).to eq(customer.first_name)
-    expect(customer['last_name']).to eq(customer.last_name)
+    expect(customer['first_name']).to eq(customer_1.first_name)
+    expect(customer['last_name']).to eq(customer_1.last_name)
   end
 end
