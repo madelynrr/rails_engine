d826705e10665a11aae2dcbfe9c707734a15fa29
Madelyn Romero <m.romero013@gmail.com>
2020-01-30 12:03:16 -0700

    Add route for customer index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         resources :items, only: [:index]
       end
       resources :items, only: [:index, :show]
+      resources :customers, only: [:index]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
