0b68531d28bd5c899fa790ba42f1199272ad9d37
Madelyn Romero <m.romero013@gmail.com>
2020-01-30 13:53:27 -0700

    Add route for invoice item show action

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index, :show]
       resources :transactions, only: [:index, :show]
-      resources :invoice_items, only: [:index]
+      resources :invoice_items, only: [:index, :show]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
