82524c721833a285c036b3eb792abbc32e0be77a
Madelyn Romero <m.romero013@gmail.com>
2020-01-30 12:30:07 -0700

    Use invoice serializer for show action

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::InvoicesController < ApplicationController
     render json: InvoiceSerializer.new(Invoice.all)
   end
 
+  def show
+    render json: InvoiceSerializer.new(Invoice.find(params[:id]))
+  end
+
 end
