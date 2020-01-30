d3a8e61613240001f1ccf82c4ea60d3caf1cad54
Madelyn Romero <m.romero013@gmail.com>
2020-01-30 13:45:33 -0700

    Create test for returning list of invoice items

---
+++ spec/requests/api/v1/invoice_item_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Invoice Items API" do
+  it "can return list of invoice items" do
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    customer_1 = create(:customer)
+    customer_2 = create(:customer)
+    item_1 = create(:item, merchant_id: merchant_1.id)
+    item_2 = create(:item, merchant_id: merchant_2.id)
+    invoice_1 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_1.id)
+    invoice_2 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+    invoice_item_1 = create(:invoice_item, item_id: item_1.id, invoice_id: invoice_1.id)
+    invoice_item_2 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_2.id)
+
+    get "/api/v1/invoice_items"
+
+    invoice_items = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(invoice_items.count).to eq(2)
+    expect(invoice_items[0]['type']).to eq('invoice_item')
+    expect(invoice_items[1]['type']).to eq('invoice_item')
+  end
+end
