c65c861f4fcd9f903d8e878f27f1d5232d7d34bb
Madelyn Romero <m.romero013@gmail.com>
2020-01-30 12:09:03 -0700

    Create test for sending single customer by id

--- spec/requests/api/v1/customer_request_spec.rb
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -14,4 +14,16 @@ describe "Customer API" do
     expect(customers[1]['type']).to eq('customer')
     expect(customers[2]['type']).to eq('customer')
   end
+
+  it "sends single customer by id" do
+    customer = create(:customer)
+
+    get "/api/v1/customers/#{customer.id}"
+
+    customer = JSON.parse(response.body)['data']['attributes']
+
+    expect(response).to be_successful
+    expect(customer['first_name']).to eq(customer.first_name)
+    expect(customer['last_name']).to eq(customer.last_name)
+  end
 end
