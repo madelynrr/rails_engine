1e227ebc026b73bee702627157ce2b2fac38e3b1
Madelyn Romero <m.romero013@gmail.com>
2020-01-30 09:41:28 -0700

    Add test for returns single item by id

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -11,8 +11,21 @@ describe "Items API" do
 
     expect(response).to be_successful
     expect(items.count).to eq(3)
-    expect(items[0]['type']).to eq("items")
-    expect(items[1]['type']).to eq("items")
-    expect(items[2]['type']).to eq("items")
+    expect(items[0]['type']).to eq("item")
+    expect(items[1]['type']).to eq("item")
+    expect(items[2]['type']).to eq("item")
+  end
+
+  it "sends a single item by its id" do
+    merchant = create(:merchant)
+    item_1 = create(:item, merchant_id: merchant.id)
+    create(:item, merchant_id: merchant.id)
+
+    get "/api/v1/items/#{item_1.id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item["id"]).to eq(item_1.id)
   end
 end
