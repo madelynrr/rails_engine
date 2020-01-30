d4e7ab2909ec97a2c5df574758f16cddbd068f8e
Madelyn Romero <m.romero013@gmail.com>
2020-01-30 12:21:17 -0700

    Create test for return list of invoices

---
+++ spec/requests/api/v1/invoice_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "Invoice API" do
+  it "can return list of invoices" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice_list = create_list(:invoice, 3, customer_id: customer.id, merchant_id: merchant.id)
+
+    get "/api/v1/invoices"
+
+    invoices = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(invoices.count).to eq(3)
+    expect(invoies[0]['type']).to eq('invoice')
+    expect(invoies[1]['type']).to eq('invoice')
+    expect(invoies[2]['type']).to eq('invoice')
+  end
+end
