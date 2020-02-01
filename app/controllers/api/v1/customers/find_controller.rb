79d75b0f80866af7c75cbf43a49f6a32b71e6d33
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 16:32:19 -0700

    Add to show action to find based on incoming parameters

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::Customers::FindController < ApplicationController
 
   def show
-    render json: CustomerSerializer.new(Customer.find(params[:id]))
+    if params[:id]
+      render json: CustomerSerializer.new(Customer.find(params[:id]))
+    elsif params[:first_name]
+      render json: CustomerSerializer.new(Customer.find_by(first_name: params[:first_name]))
+    elsif params[:last_name]
+      render json: CustomerSerializer.new(Customer.find_by(last_name: params[:last_name]))
+    end
   end
 
   def index
