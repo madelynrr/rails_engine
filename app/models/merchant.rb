08b8bcf1bee4e93c9c9d066c0becceac2a82bd14
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 00:15:26 -0700

    Create active record call for top variable number of merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,8 +5,13 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
 
-  def top_by_revenue
-    
+  def self.top_by_revenue(quantity)
+    Merchant.joins(invoices: [:invoice_items, :transactions])
+    .select("merchants.*, SUM((invoice_items.quantity) * (invoice_items.unit_price)) AS total_revenue")
+    .group("merchants.id")
+    .merge(Transaction.successful)
+    .order(total_revenue: :desc)
+    .limit(quantity)
   end
 
 end
