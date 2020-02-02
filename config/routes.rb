48f83b6ef5c489ca238c660f320dfa77676ce165
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 20:46:24 -0700

    Create route for favorite merchant by customer

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       resources :customers, only: [:index, :show] do
         resources :invoices, only: [:index]
         resources :transactions, only: [:index]
+        get 'favorite_merchant', to: 'customers#favorite_merchant'
       end
       resources :invoices, only: [:index, :show]
       resources :transactions, only: [:index, :show]
