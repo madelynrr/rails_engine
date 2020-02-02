33ad5b0f8555cc21cf79dbd527c4dfb8a79a2ae3
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 13:22:07 -0700

    Rename from second merchant controller under namespace

--- app/controllers/api/v1/merchants/merchant_revenue_controller.rb
+++ app/controllers/api/v1/merchants/merchant_revenue_controller.rb
@@ -1,8 +1,12 @@
-class Api::V1::Merchants::MerchantsController < ApplicationController
+class Api::V1::Merchants::MerchantRevenueController < ApplicationController
 
   def index
     merchants = Merchant.top_by_revenue(params[:quantity])
     render json: MerchantSerializer.new(merchants)
+  end
+
+  def show
+    
   end
 
 end
