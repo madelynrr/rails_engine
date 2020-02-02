d9f6017fa2fde2f88388cb661f604025efcfd439
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 13:23:13 -0700

    Add invoices to test for revenue by date

--- spec/requests/api/v1/business/business_intelligence_endpoints_spec.rb
+++ spec/requests/api/v1/business/business_intelligence_endpoints_spec.rb
@@ -76,7 +76,6 @@ describe "Business Intelligence API" do
   end
 
   it "returns total revenue for a given date across all merchants" do
-    skip
     customer_1 = create(:customer)
     merchant_1 = create(:merchant)
     merchant_2 = create(:merchant)
@@ -84,20 +83,22 @@ describe "Business Intelligence API" do
     item_1 = create(:item, merchant_id: merchant_1.id)
     item_2 = create(:item, merchant_id: merchant_2.id)
     item_3 = create(:item, merchant_id: merchant_3.id)
-    invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id)
-    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id)
-    invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id)
+    invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id, created_at: "2019-02-01 23:36:19 UTC")
+    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id, created_at: "2020-02-01 23:36:19 UTC")
+    invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id, created_at: "2020-02-01 23:36:19 UTC")
     invoice_item_1 = create(:invoice_item, item_id: item_1.id, invoice_id: invoice_1.id, unit_price: 1, quantity: 2)
     invoice_item_2 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_2.id, unit_price: 1, quantity: 3)
-    invoice_item_3 = create(:invoice_item, item_id: item_3.id, invoice_id: invoice_3.id, unit_price: 1, quantity: 4)
+    invoice_item_3 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_2.id, unit_price: 1, quantity: 4)
+    invoice_item_4 = create(:invoice_item, item_id: item_3.id, invoice_id: invoice_3.id, unit_price: 1, quantity: 5)
     transaction_1 = create(:transaction, invoice_id: invoice_1.id, result: "success")
-    transaction_1 = create(:transaction, invoice_id: invoice_2.id, result: "success")
-    transaction_1 = create(:transaction, invoice_id: invoice_3.id, result: "failed")
+    transaction_2 = create(:transaction, invoice_id: invoice_2.id, result: "success")
+    transaction_3 = create(:transaction, invoice_id: invoice_2.id, result: "success")
+    transaction_4 = create(:transaction, invoice_id: invoice_3.id, result: "failed")
 
-    get "/api/v1/merchants/revenue?date="
+    get "/api/v1/merchants/revenue?date=2020-02-01 23:36:19 UTC"
 
+    revenue = JSON.parse(response.body)
 
-
-
+    expect(revenue).to eq(7.0)
   end
 end
