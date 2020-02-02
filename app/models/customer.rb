8966e3af0abfa65cec2ae42d71bdf151689dc954
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 21:29:48 -0700

    Create model method to find favorite merchant by customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,13 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def fav_merchant
-    
+    x = Merchant.joins(invoices: :transactions)
+    .select("merchants.*, COUNT(transactions.id) AS total_transactions")
+    .group("merchants.id")
+    .where(transactions: {result: "success"})
+    .where(invoices: {customer_id: "#{self.id}"})
+    .order(total_transactions: :desc)
+    .limit(1)
+    .first
   end
 end
