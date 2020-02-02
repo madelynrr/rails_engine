1b62de24a2b5b63ce8a3816912c5c9c2d2d97d6d
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 20:46:39 -0700

    Create dream driven code for finding favorite merchant by customer

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -6,4 +6,10 @@ class Api::V1::CustomersController < ApplicationController
   def show
     render json: CustomerSerializer.new(Customer.find(params[:id]))
   end
+
+  def favorite_merchant
+    customer = Customer.find(params[:customer_id])
+    favorite = customer.fav_merchant
+    render json: MerchantSerializer(Merchant.find(favorite.id))
+  end
 end
