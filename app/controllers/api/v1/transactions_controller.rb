da979a33689d8dce68c71c11d6ef0b8bfb6e5b7a
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 19:01:52 -0700

    Add logic for returning transactions by customer

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::TransactionsController < ApplicationController
 
   def index
-    render json: TransactionSerializer.new(Transaction.all)
+    if params[:customer_id]
+      customer = Customer.find(params[:customer_id])
+      render json: TransactionSerializer.new(customer.transactions)
+    else
+      render json: TransactionSerializer.new(Transaction.all)
+    end
   end
 
   def show
