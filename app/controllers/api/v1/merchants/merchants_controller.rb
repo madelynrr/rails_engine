7a27f922a15c412b3aed0d46318d441723966134
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 23:44:15 -0700

    Dream drive calling top variable number of merchants by total revenue

--- app/controllers/api/v1/merchants/merchants_controller.rb
+++ app/controllers/api/v1/merchants/merchants_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::Merchants::MerchantsController < ApplicationController
-  
+
+  def most_revenue
+    merchants = Merchant.top_by_revenue(params[:quantity])
+    render json: MerchantSerializer.new(merchants)
+  end
+
 end
