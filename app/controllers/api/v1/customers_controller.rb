b95f272d424815e79a21788a6db2ab6ad42026ce
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 21:30:04 -0700

    Create favorite merchant action

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::CustomersController < ApplicationController
 
   def favorite_merchant
     customer = Customer.find(params[:customer_id])
-    favorite = customer.fav_merchant
-    render json: MerchantSerializer(Merchant.find(favorite.id))
+    favorite_id = customer.fav_merchant.id
+    render json: MerchantSerializer.new(Merchant.find(favorite_id))
   end
 end
