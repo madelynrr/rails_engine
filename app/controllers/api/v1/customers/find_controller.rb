5d4d974308b09b93c12338df5664f12909fb76bb
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 17:05:08 -0700

    Add logic to find first instance by created at and updated at

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::Customers::FindController < ApplicationController
       render json: CustomerSerializer.new(Customer.find_by(first_name: params[:first_name]))
     elsif params[:last_name]
       render json: CustomerSerializer.new(Customer.find_by(last_name: params[:last_name]))
+    elsif params[:created_at]
+      render json: CustomerSerializer.new(Customer.find_by(created_at: params[:created_at]))
+    elsif params[:updated_at]
+      render json: CustomerSerializer.new(Customer.find_by(updated_at: params[:updated_at]))
     end
   end
 
