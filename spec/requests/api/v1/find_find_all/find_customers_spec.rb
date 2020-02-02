c51229d103a73e86e49cdd3ef1e60ceec9d8617a
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 17:27:54 -0700

    Create tests for find all by first name, last name, created at, updated at

--- spec/requests/api/v1/find_find_all/find_customers_spec.rb
+++ spec/requests/api/v1/find_find_all/find_customers_spec.rb
@@ -72,4 +72,64 @@ describe "Customer API" do
     expect(customer['id']).to eq(customer_1.id)
   end
 
+  it "can find all customers by first name" do
+    customer_1 = create(:customer, first_name: "Bob")
+    customer_2 = create(:customer, first_name: "Bob")
+    customer_3 = create(:customer, first_name: "Billy")
+
+    get "/api/v1/customers/find_all?first_name=#{customer_1.first_name}"
+
+    customers = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(customers.count).to eq(2)
+    expect(customers[0]['attributes']['id']).to eq(customer_1.id)
+    expect(customers[1]['attributes']['id']).to eq(customer_2.id)
+  end
+
+  it "can find all instances by last name" do
+    customer_1 = create(:customer, last_name: "Smith")
+    customer_2 = create(:customer, last_name: "Smith")
+    customer_3 = create(:customer, last_name: "Smitha")
+
+    get "/api/v1/customers/find_all?last_name=#{customer_1.last_name}"
+
+    customers = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(customers.count).to eq(2)
+    expect(customers[0]['attributes']['id']).to eq(customer_1.id)
+    expect(customers[1]['attributes']['id']).to eq(customer_2.id)
+  end
+
+  it "can find all instances by created at" do
+    customer_1 = create(:customer, created_at: "2020-02-01 23:36:19 UTC")
+    customer_2 = create(:customer, created_at: "2020-02-01 23:36:19 UTC")
+    customer_3 = create(:customer, created_at: "2019-02-01 23:36:19 UTC")
+
+    get "/api/v1/customers/find_all?created_at='2020-02-01 23:36:19 UTC'"
+
+    customers = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(customers.count).to eq(2)
+    expect(customers[0]['attributes']['id']).to eq(customer_1.id)
+    expect(customers[1]['attributes']['id']).to eq(customer_2.id)
+  end
+
+  it "can find all instance by updated at" do
+    customer_1 = create(:customer, updated_at: "2020-02-01 23:36:19 UTC")
+    customer_2 = create(:customer, updated_at: "2020-02-01 23:36:19 UTC")
+    customer_3 = create(:customer, updated_at: "2019-02-01 23:36:19 UTC")
+
+    get "/api/v1/customers/find_all?updated_at='2020-02-01 23:36:19 UTC'"
+
+    customers = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(customers.count).to eq(2)
+    expect(customers[0]['attributes']['id']).to eq(customer_1.id)
+    expect(customers[1]['attributes']['id']).to eq(customer_2.id)
+  end
+
 end
