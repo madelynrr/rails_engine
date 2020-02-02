27d0162a6aab7048241b36cbc933c43dd72fd531
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 23:36:36 -0700

    Create route for variable number of merchants by total revenue

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,33 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+
       namespace :customers do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
         get 'random', to: 'random#show'
       end
+
+      namespace :merchants do
+        get '/most_revenue', to: 'merchants#most_revenue'
+      end
+
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
       end
+
       resources :items, only: [:index, :show]
+
       resources :customers, only: [:index, :show] do
         resources :invoices, only: [:index]
         resources :transactions, only: [:index]
         get 'favorite_merchant', to: 'customers#favorite_merchant'
       end
+
       resources :invoices, only: [:index, :show]
+
       resources :transactions, only: [:index, :show]
+      
       resources :invoice_items, only: [:index, :show]
 
 
