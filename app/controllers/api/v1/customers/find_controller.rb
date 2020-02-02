4a036c8966ab487c0510f6a45dcc37d588f3bc41
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 17:28:11 -0700

    Add logic to find all by first name, last name, created at, updated at

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -15,7 +15,17 @@ class Api::V1::Customers::FindController < ApplicationController
   end
 
   def index
-    render json: CustomerSerializer.new(Customer.where(id: params[:id]))
+    if params[:id]
+      render json: CustomerSerializer.new(Customer.where(id: params[:id]))
+    elsif params[:first_name]
+      render json: CustomerSerializer.new(Customer.where(first_name: params[:first_name]))
+    elsif params[:last_name]
+      render json: CustomerSerializer.new(Customer.where(last_name: params[:last_name]))
+    elsif params[:created_at]
+      render json: CustomerSerializer.new(Customer.where(created_at: params[:created_at]))
+    elsif params[:updated_at]
+      render json: CustomerSerializer.new(Customer.where(updated_at: params[:updated_at]))
+    end
   end
 
 end
