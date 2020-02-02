5f658f6678124a1c402bae92ba0d8a2df929fe32
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 13:47:10 -0700

    Dream drive return total revenue by date action

--- app/controllers/api/v1/merchants/merchant_revenue_controller.rb
+++ app/controllers/api/v1/merchants/merchant_revenue_controller.rb
@@ -6,7 +6,15 @@ class Api::V1::Merchants::MerchantRevenueController < ApplicationController
   end
 
   def show
-    
+    revenue = Invoice.total_revenue(params[:created_at])
+    render json: RevenueSerializer.new(revenue)
   end
+
+  # x = Invoice.joins(:transactions)
+  # .select("invoice_items.quantity * invoice_items.unit_price AS total_revenue")
+  # .merge(Transaction.successful)
+  # .where("invoices.created_at: 2020-02-01 23:36:19 UTC")
+  # .group("invoices.created_at")
+  # require "pry"; binding.pry
 
 end
