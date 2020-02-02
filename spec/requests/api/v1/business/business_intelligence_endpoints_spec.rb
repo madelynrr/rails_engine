8742362b58bf959537f7b9debdb3b2692d1ad1ab
Madelyn Romero <m.romero013@gmail.com>
2020-02-01 23:36:00 -0700

    Create test for top variable number of merchants by total revenue

--- spec/requests/api/v1/business/business_intelligence_endpoints_spec.rb
+++ spec/requests/api/v1/business/business_intelligence_endpoints_spec.rb
@@ -29,4 +29,49 @@ describe "Business Intelligence API" do
     expect(response).to be_successful
     expect(favorite_merchant['attributes']['id']).to eq(merchant_1.id)
   end
+
+  it "returns top variable number of merchants with top revenue" do
+    customer_1 = create(:customer)
+    customer_2 = create(:customer)
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    merchant_3 = create(:merchant)
+    merchant_4 = create(:merchant)
+    item_1 = create(:item, merchant_id: merchant_1.id)
+    item_2 = create(:item, merchant_id: merchant_2.id)
+    item_3 = create(:item, merchant_id: merchant_3.id)
+    item_4 = create(:item, merchant_id: merchant_4.id)
+    invoice_1 = create(:invoice, customer_id: customer_2.id, merchant_id: merchant_1.id)
+    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id)
+    invoice_3 = create(:invoice, customer_id: customer_2.id, merchant_id: merchant_2.id)
+    invoice_4 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id)
+    invoice_5 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id)
+    invoice_6 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id)
+    invoice_7 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id)
+    invoice_8 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id)
+    invoice_item_1 = create(:invoice_item, item_id: item_1.id, invoice_id: invoice_1.id, unit_price: 1)
+    invoice_item_2 = create(:invoice_item, item_id: item_1.id, invoice_id: invoice_2.id, unit_price: 1)
+    invoice_item_3 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_3.id, unit_price: 1)
+    invoice_item_4 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_4.id, unit_price: 1)
+    invoice_item_5 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_5.id, unit_price: 1)
+    invoice_item_6 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_6.id, unit_price: 1)
+    invoice_item_7 = create(:invoice_item, item_id: item_3.id, invoice_id: invoice_7.id, unit_price: 1)
+    invoice_item_8 = create(:invoice_item, item_id: item_4.id, invoice_id: invoice_8.id, unit_price: 1)
+    transaction_1 = create(:transaction, invoice_id: invoice_1.id, result: "success")
+    transaction_2 = create(:transaction, invoice_id: invoice_2.id, result: "success")
+    transaction_3 = create(:transaction, invoice_id: invoice_3.id, result: "success")
+    transaction_4 = create(:transaction, invoice_id: invoice_4.id, result: "failed")
+    transaction_5 = create(:transaction, invoice_id: invoice_5.id, result: "failed")
+    transaction_6 = create(:transaction, invoice_id: invoice_6.id, result: "failed")
+    transaction_7 = create(:transaction, invoice_id: invoice_7.id, result: "failed")
+    transaction_8 = create(:transaction, invoice_id: invoice_8.id, result: "failed")
+
+    get "/api/v1/merchants/most_revenue?quantity=2"
+
+    top_merchants = JSON.parse(response.body)['data']
+
+    expect(top_merchants.count).to eq(2)
+    expect(top_merchants[0]['attributes']['id']).to eq(merchant_1.id)
+    expect(top_merchants[1]['attributes']['id']).to eq(merchant_2.id)
+  end
 end
