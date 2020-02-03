f7813da575635a7ad47aa8d9f62122409dd39e6b
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 18:50:54 -0700

    Add logic for invoice items by invoice

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::InvoiceItemsController < ApplicationController
 
 
   def index
-    render json: InvoiceItemSerializer.new(InvoiceItem.all)
+    if params[:invoice_id]
+      invoice = Invoice.find(params[:invoice_id])
+      render json: InvoiceItemSerializer.new(invoice.invoice_items)
+    else
+      render json: InvoiceItemSerializer.new(InvoiceItem.all)
+    end
   end
 
   def show
