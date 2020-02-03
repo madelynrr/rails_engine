961f0ac48ce3cf158fe202984eb6857e186bef18
Madelyn Romero <m.romero013@gmail.com>
2020-02-03 05:29:38 -0700

    Add logic for all find and find all endpoints

---
+++ app/controllers/api/v1/merchants/find_merchants_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::Merchants::FindMerchantsController < ApplicationController
+
+  def show
+    if params[:id]
+      render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    elsif params[:name]
+      render json: MerchantSerializer.new(Merchant.find_by(name: params[:name]))
+    elsif params[:created_at]
+      render json: MerchantSerializer.new(Merchant.find_by(created_at: params[:created_at]))
+    elsif params[:updated_at]
+      render json: MerchantSerializer.new(Merchant.find_by(updated_at: params[:updated_at]))
+    end
+  end
+
+  def index
+    if params[:id]
+      render json: MerchantSerializer.new(Merchant.where(id: params[:id]))
+    elsif params[:name]
+      render json: MerchantSerializer.new(Merchant.where(name: params[:name]))
+    elsif params[:created_at]
+      render json: MerchantSerializer.new(Merchant.where(created_at: params[:created_at]))
+    elsif params[:updated_at]
+      render json: MerchantSerializer.new(Merchant.where(updated_at: params[:updated_at]))
+    end
+  end
+end
