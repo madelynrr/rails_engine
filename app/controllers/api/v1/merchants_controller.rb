551132f89fd262de72551103fd50c6aa202fa089
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 19:22:03 -0700

    Add logic for merchant by invoice

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
-    render json: MerchantSerializer.new(Merchant.all)
+    if params[:invoice_id]
+      invoice = Invoice.find(params[:invoice_id])
+      render json: MerchantSerializer.new(invoice.merchant)
+    else
+      render json: MerchantSerializer.new(Merchant.all)
+    end
   end
 
   def show
