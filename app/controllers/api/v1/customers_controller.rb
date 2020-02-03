d2ab1998fea94c718c559c266edf5f6c9b7989e9
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 19:13:20 -0700

    Add logic for customer by invoice

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::CustomersController < ApplicationController
   def index
-    render json: CustomerSerializer.new(Customer.all)
+    if params[:invoice_id]
+      invoice = Invoice.find(params[:invoice_id])
+      render json: CustomerSerializer.new(invoice.customer)
+    else
+      render json: CustomerSerializer.new(Customer.all)
+    end
   end
 
   def show
     render json: CustomerSerializer.new(Customer.find(params[:id]))
   end
-  
+
 end
