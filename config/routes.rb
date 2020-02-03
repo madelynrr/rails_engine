3df001665f28836e5b11301020cb24de5934f817
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 19:03:52 -0700

    Create route for items by invoice

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
       resources :invoices, only: [:index, :show] do
         resources :transactions, only: [:index]
         resources :invoice_items, only: [:index]
+        resources :items, only: [:index]
       end
 
       resources :transactions, only: [:index, :show]
