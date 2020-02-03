dbf80debfb2b77aac1e13de6843d77cfb863291a
Madelyn Romero <m.romero013@gmail.com>
2020-02-03 03:37:28 -0700

    Add logic for all find by attributes endpoints

--- app/controllers/api/v1/invoices/find_invoices_controller.rb
+++ app/controllers/api/v1/invoices/find_invoices_controller.rb
@@ -1,3 +1,23 @@
 class Api::V1::Invoices::FindInvoicesController < ApplicationController
-  
+
+  def show
+    if params[:id]
+      render json: InvoiceSerializer.new(Invoice.find(params[:id]))
+    elsif params[:customer_id]
+      render json: InvoiceSerializer.new(Invoice.find_by(customer_id: params[:customer_id]))
+    elsif params[:merchant_id]
+      render json: InvoiceSerializer.new(Invoice.find_by(merchant_id: params[:merchant_id]))
+    elsif params[:status]
+      render json: InvoiceSerializer.new(Invoice.find_by(status: params[:status]))
+    elsif params[:created_at]
+      render json: InvoiceSerializer.new(Invoice.find_by(created_at: params[:created_at]))
+    elsif params[:updated_at]
+      render json: InvoiceSerializer.new(Invoice.find_by(updated_at: params[:updated_at]))
+    end
+  end
+
+  def index
+    
+  end
+
 end
