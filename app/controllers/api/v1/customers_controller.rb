e1f45ee8e79ff89a777f7b7a1b6b00c409a92213
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 20:06:19 -0700

    Refactor so customer by invoice is a show action

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::CustomersController < ApplicationController
   def index
+      render json: CustomerSerializer.new(Customer.all)
+  end
+
+  def show
     if params[:invoice_id]
       invoice = Invoice.find(params[:invoice_id])
       render json: CustomerSerializer.new(invoice.customer)
     else
-      render json: CustomerSerializer.new(Customer.all)
+      render json: CustomerSerializer.new(Customer.find(params[:id]))
     end
-  end
-
-  def show
-    render json: CustomerSerializer.new(Customer.find(params[:id]))
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
         resources :transactions, only: [:index]
         resources :invoice_items, only: [:index]
         resources :items, only: [:index]
-        get '/customer', to: 'customers#index'
+        get '/customer', to: 'customers#show'
         get '/merchant', to: 'merchants#index'
       end
 
