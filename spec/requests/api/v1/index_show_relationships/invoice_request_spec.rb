2bfdbb3983725160bc552906b34d71cbb38a0698
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 18:38:02 -0700

    Create test to return transactions based on invoice

--- spec/requests/api/v1/index_show_relationships/invoice_request_spec.rb
+++ spec/requests/api/v1/index_show_relationships/invoice_request_spec.rb
@@ -25,8 +25,27 @@ describe "Invoice API" do
     get "/api/v1/invoices/#{invoice_1.id}"
 
     invoice = JSON.parse(response.body)['data']['attributes']
-    
+
     expect(response).to be_successful
     expect(invoice['id']).to eq(invoice_1.id)
+  end
+
+  it "can return collection of transactions by invoice" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice_1 = create(:invoice, customer_id: customer.id, merchant_id: merchant.id)
+    invoice_2 = create(:invoice, customer_id: customer.id, merchant_id: merchant.id)
+    transaction_1 = create(:transaction, invoice_id: invoice_1.id)
+    transaction_2 = create(:transaction, invoice_id: invoice_1.id)
+    transaction_3 = create(:transaction, invoice_id: invoice_2.id)
+
+    get "/api/v1/invoices/#{invoice_1.id}/transactions"
+
+    transactions = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(transactions.count).to eq(2)
+    expect(transactions[0]['attributes']['invoice_id']).to eq(invoice_1.id)
+    expect(transactions[1]['attributes']['invoice_id']).to eq(invoice_1.id)
   end
 end
