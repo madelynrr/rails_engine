63c45e5ba67aad47e2b996146d02dc1e3bab279a
Madelyn Romero <m.romero013@gmail.com>
2020-02-02 18:40:47 -0700

    Add logic to return based on invoice

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -4,6 +4,9 @@ class Api::V1::TransactionsController < ApplicationController
     if params[:customer_id]
       customer = Customer.find(params[:customer_id])
       render json: TransactionSerializer.new(customer.transactions)
+    elsif params[:invoice_id]
+      invoice = Invoice.find(params[:invoice_id])
+      render json: TransactionSerializer.new(invoice.transactions)
     else
       render json: TransactionSerializer.new(Transaction.all)
     end
